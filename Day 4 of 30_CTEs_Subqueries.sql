e47be1f0082684709340f5d386a22175580fff50
Suparna Chowdhury <suchow32@gmail.com>
2026-07-15 21:06:48 -0400

    Day 4 update

--- Day 4 of 30_CTEs_Subqueries.sql
+++ Day 4 of 30_CTEs_Subqueries.sql
@@ -35,12 +35,15 @@ INSERT INTO sales VALUES
 
 -- 2. For each salesperson, count the number of high-value sales (sales greater than $1,000).
 
-Medium
-Find the salespeople whose total sales are greater than the average total sales of all salespeople.
 
-For each salesperson, return their total sales, average sale amount, and total value of high-value sales (sales greater than $1,000) in a single query.
+-- Medium
 
-Create a report that displays each salesperson's February sales total as a separate column using conditional aggregation.
+-- 1. Find the salespeople whose total sales are greater than the average total sales of all salespeople.
+
+
+-- 2. For each salesperson, return their total sales, average sale amount, and total value of high-value sales (sales greater than $1,000) in a single query.
+
+-- 3. Create a report that displays each salesperson's February sales total as a separate column using conditional aggregation.
 
 Bonus Challenge
 Write a single query that returns each salesperson_id, their total sales, average sale amount, number of high-value sales (greater than $1,000), and their rank based on total sales. Your solution must use a CTE, conditional aggregation, and a window function, and it should not use any nested subqueries.
